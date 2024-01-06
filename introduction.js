85ee7c4304ae21ad6632e8a6b3a6505659babc24
ShatakshiN <147754096+ShatakshiN@users.noreply.github.com>
2024-01-06 15:55:28 +0530

    Create introduction.js
    first demo of nodejs modules

---
+++ introduction.js
@@ -0,0 +1,14 @@
+//console.log('hello world');
+const fs = require('fs'); //This line imports the built-in fs module in Node.js, which stands for "file system." This module provides a set of methods for working with the file system, including reading and writing files.
+
+/* fs.writeFileSync("hello.text",'hello world'); 
+
+fs.writeFileSync is a method from the fs module.
+
+syntax  = fs.writeFileSync('file name', 'data to be stored in the file')
+
+"hello.text": This is the name of the file to which the data will be written. In this case, it's "hello.text".
+ If the file doesn't exist, it will be created. If it already exists, its content will be replaced.
+
+ 'hello world': This is the data that will be written to the file. In this case, it's the string "hello world."
+ */
